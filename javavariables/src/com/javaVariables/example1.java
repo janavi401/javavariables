d7448d24e990faed85a0d43193a3e66219a7d9fa
Janavi Reddy <Janavi Reddy@DESKTOP-73OQVUV>
2022-02-07 10:45:25 +0530

    variables file added

---
+++ javavariables/src/com/javaVariables/example1.java
@@ -0,0 +1,36 @@
+package com.javaVariables;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Write a java program to print student information using java variables and literals
+		
+				//variable declaration
+				int studentId;
+				String studentName;
+				float studentMarks;
+				String studentAddress;
+				
+				//variable initialization
+				studentId = 3020;
+				studentName = "janavi";
+				studentMarks = 58.45f;
+				studentAddress = "karimnagar";
+				
+				System.out.print("Student Id: ");
+				System.out.println(studentId);
+				
+				System.out.print("Student Name: ");
+				System.out.println(studentName);
+				
+				System.out.print("Student Marks: ");
+				System.out.println(studentMarks);
+				
+				System.out.print("Student Address: ");
+				System.out.println(studentAddress);
+				
+
+	}
+
+}
